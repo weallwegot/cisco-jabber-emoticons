9d088eca576236eb577e4e0c19e22ef7784de597
Aaryn Gloster <aaryngloster@gmail.com>
2017-08-16 01:07:38 -0400

    Download images from base64 data from json

---
+++ hi.py
@@ -0,0 +1,1 @@
+print "DOES THIS WORK???"
--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 # webscraping library parser
+print "HI!!!!!!!!!!!!!!!!!!!!!!!!!!"
 import lxml.html
 import requests
 from collections import OrderedDict
@@ -7,7 +8,7 @@ import json
 # webscraping library w/ easier syntax
 from bs4 import BeautifulSoup, SoupStrainer
 
-
+print "#$UJ#(*H$OI#NO$NONIO#N$O@N#O$)"
 
 
 headers = {'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.112 Safari/534.30"}
@@ -19,7 +20,7 @@ except requests.exceptions.RequestException as e:
 except requests.exceptions.ConnectionError as e:
 	print e
 emoji_html = response.content
-# sub set of the full page that only includes tags with attributes of border=1. 
+# sub set of the full page that only includes tags with attributes of border=1.
 # (this is the giant table with all the emojis)
 only_the_large_important_table = SoupStrainer(attrs={"border":"1"})
 
@@ -53,11 +54,5 @@ for tr_tag in all_table_rows:
 						super_list.append(good_tuple)
 		except:
 			continue
-
-
-#print(str(super_dictionary.keys()))
 with open('pics_data.json','w') as outfile:
 	json.dump(OrderedDict(super_list),outfile)
-
-
-
