9bf9128963b16503c76c4a0b192bfa5bbe93dfa3
olutosinsonuyi <tosin.sonuyi@gmail.com>
2017-08-16 19:30:32 -0400

    getting rid of special chars in names encoding issues

--- download_imgs.py
+++ download_imgs.py
@@ -1,5 +1,6 @@
 import json
 import base64
+import os
 
 with open('pics_data.json','r') as infile:
 	imgs = json.load(infile)
@@ -8,7 +9,11 @@ infile.close()
 for key, value in imgs.iteritems():
 	try:
 		img_data = value.split("base64,")[1]
-		with open(key + ".png", "wb") as fh:
+		key_transformed = key.replace('(','').replace(')','').replace('&','').replace("\'",'').encode("utf8").lower()
+
+		img_file_path = 'imgs'+ os.sep + key_transformed + '.png' 
+		print img_file_path
+		with open(img_file_path, "wb") as fh:
 		    fh.write(base64.b64decode(img_data))
 		fh.close()
 	except Exception as e:
