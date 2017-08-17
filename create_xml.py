337297e40350d922e0a3d5e59327b59a01164b3f
olutosinsonuyi <tosin.sonuyi@gmail.com>
2017-08-16 20:01:05 -0400

    ignore the .dstore file

--- create_xml.py
+++ create_xml.py
@@ -8,18 +8,19 @@ order_number = 1
 for path, dirs, files in os.walk("resized_imgs"):
     for file_name in files:
         ext = file_name[-3:].lower()
-        file_no_ext = file_name[:-4]
-        file_no_ext = file_no_ext.replace('(','').replace(')','').replace('&','').lower().decode('utf-8')
-        print file_no_ext
-        curr_emoticon_def = ET.SubElement(root,"emoticon",
-        	defaultKey=file_no_ext,
-        	image=file_name.decode('utf-8'),
-        	text=file_no_ext,
-        	order=str(order_number))
-        order_number += 1
+        if ext == "png":
+	        file_no_ext = file_name[:-4]
+	        file_no_ext = file_no_ext.replace('(','').replace(')','').replace('&','').lower().decode('utf-8')
+	        print file_no_ext
+	        curr_emoticon_def = ET.SubElement(root,"emoticon",
+	        	defaultKey=file_no_ext,
+	        	image=file_name.decode('utf-8'),
+	        	text=file_no_ext,
+	        	order=str(order_number))
+	        order_number += 1
 
 # https://stackoverflow.com/questions/28813876/how-do-i-get-pythons-elementtree-to-pretty-print-to-an-xml-file
 xmlstr = minidom.parseString(ET.tostring(root)).toprettyxml(indent="   ")
-with open("emoticonDef.xml", "w") as f:
+with open("emoticonDefs.xml", "w") as f:
     f.write(xmlstr.encode('utf-8'))
 
